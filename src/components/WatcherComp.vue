c27c7cd2b0b90e99fd57787d8cb1210e0554ad15
neel <neel.citrusbug@gmail.com>
2022-07-21 19:59:48 +0530

    immediate watchers and deep watchers

--- src/components/WatcherComp.vue
+++ src/components/WatcherComp.vue
@@ -6,6 +6,13 @@
             <button @click="volume-=2">Decrease</button>
             <button @click="volume+=2">Increase</button>
         </div>
+        <input type="text" v-model="movie">
+        <input type="text" v-model="movieInfo.title">
+        <input type="text" v-model="movieInfo.actor">
+        <div>
+            <button @click="movieList.push('wonder women')">Add movie</button>
+            <!-- <button @click="movieList = movieList.concat(['wonder women'])">Add movie</button> -->
+        </div>
     </div>
 </template>
 
@@ -15,6 +22,12 @@
         data(){
             return{
                 volume:0,
+                movie:'bat',
+                movieInfo:{
+                    title:'',
+                    actor:''
+                },
+                movieList:['Batman','Superman']
             }
         },
         watch:{
@@ -25,7 +38,27 @@
                 if(newValue > 20 || newValue < 0){
                     this.volume = oldValue
                 }
-            }
+            },
+            movie:{
+                handler(newValue){
+                    console.log(`api called - movie is ${newValue}`)
+                },
+                immediate:true,
+            },
+            movieInfo:{
+                handler(newValue){
+                    console.log(`api called - movie title is ${newValue.title} and actor is ${newValue.actor}`)
+                },
+                deep:true,
+                immediate:true,
+            },
+            movieList:{
+                handler(newValue){
+                    console.log(`updated list is ${newValue}`)
+                },
+                deep:true,
+                immediate:true,
+            }  
         }
     }
 </script>
